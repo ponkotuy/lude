290fdc5d327239b410a1f3b3832894f127779dfc
ponkotuy <web@ponkotuy.com>
2015-06-10 04:47:04 +0900

    Add FlatMap etc...

--- Main.hs
+++ Main.hs
@@ -1,30 +1,77 @@
 
-import Prelude (print, Show, Eq, (+), ($), (==), (&&))
+import Prelude (print, Show, Eq, (.), (+), ($), (==), (&&), Char, Int, Bool(True, False), not)
 
 class Functor f where
     map :: (a -> b) -> f a -> f b
 
-data Option a = Some a | None deriving (Show, Eq)
+class FlatMap f where
+    flatMap :: (a -> f b) -> f a -> f b
+
+class Empty a where
+    empty :: a
+
+class (Eq a, Empty a) => IsEmpty a where
+    isEmpty :: a -> Bool
+    isEmpty x = x == empty
+
+    nonEmpty :: a -> Bool
+    nonEmpty = not . isEmpty
+
+listJoin :: List a -> List a -> List a
+listJoin Nil Nil = Nil
+listJoin Nil xs = xs
+listJoin xs Nil = xs
+listJoin (Cons x xs) a = Cons x (xs ++ a)
+
+class Traversable t where
+    toList :: (t a) -> List a
+    (++) :: (t a) -> (t a) -> List a
+    xs ++ ys = listJoin (toList xs) (toList ys)
+
+data Option a = Some a | None
+                deriving Show
 
 instance Functor Option where
     map _ None = None
     map f (Some a) = Some (f a)
 
-data List a = Cons a (List a) | Nil deriving (Show, Eq)
+instance FlatMap Option where
+    flatMap _ None = None
+    flatMap f (Some a) = f a
+
+instance Empty (Option a) where
+    empty = None
+
+instance Eq (Option a) where
+    None == None = True
+    None == Some x = False
+    Some x == None = False
+    Some x == Some y = True
+
+instance IsEmpty (Option a)
+
+data List a = Cons a (List a) | Nil
+              deriving (Show, Eq)
 
 instance Functor List where
     map _ Nil = Nil
     map f (Cons x xs) = Cons (f x) (map f xs)
 
--- infixr 4 :
+instance Traversable List where
+    toList xs = xs
 
--- (:) :: a -> List a -> List a
--- (:) = Cons
+instance FlatMap List where
+    flatMap _ Nil = Nil
+    flatMap f (Cons x xs) = (f x) ++ (flatMap f xs)
+
+instance Empty (List a) where
+    empty = Nil
 
 main = print $ listMap && opt
     where
-      opt = mapNone && mapSome
-      mapNone = (map (+ 1) None) == None
+      opt = mapNone && mapSome && optNonEmpty
+      mapNone = isEmpty $ map (+ 1) None
       mapSome = (map (+ 1) (Some 1)) == (Some 2)
+      optNonEmpty = nonEmpty $ Some 1
       list = Cons 1 (Cons 2 (Cons 3 Nil))
       listMap = (map (+ 1) list) == (Cons 2 (Cons 3 (Cons 4 Nil)))
